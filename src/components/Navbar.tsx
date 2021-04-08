3fac075a470e20021528a9cb370fad17201d6afd
Shlok D. Mange <shlok_mange@protonmail.com>
2021-04-08 15:48:41 +0400

    Navbar transitions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar: React.FC = () => {
 
-	const [dropdown, setDropdown] = React.useState(true)
+	const [dropdown, setDropdown] = React.useState(false)
 
 	return(
-		<nav style={{fontFamily:"'Cairo', sans-serif"}} className="space-y-4">
+		<nav style={{fontFamily:"'Cairo', sans-serif"}} className="space-y-4 px-8 py-4 lg:px-16">
 			<div className="flex items-center justify-between">
 				<h1 className="text-transparent tracking-wider bg-clip-text bg-gradient-to-br from-cyan-300 to-fuchsia-500 font-bold text-3xl">
 					vybe
 				</h1>
-				<div>
+				<div className="sm:hidden">
 					<p className="text-violet-700 opacity-90 font-bold text-lg">
 						<FontAwesomeIcon 
-							icon={faBars}
+							icon={dropdown ? faTimes : faBars}
 							onClick={() => setDropdown(!dropdown)}
+							className="cursor-pointer"
 						/>
 					</p>
 				</div>
@@ -30,7 +31,7 @@ const Navbar: React.FC = () => {
 
 const Dropdown: React.FC = () => {
 	return(
-		<div className="space-y-2 transition duration-150 text-lg text-transparent tracking-wider bg-clip-text bg-gradient-to-tl from-cyan-300 to-fuchsia-500 font-bold">
+		<div className="space-y-2 transition ease-out duration-150 text-lg text-transparent tracking-wider bg-clip-text bg-gradient-to-tl from-cyan-300 to-fuchsia-500 font-bold">
 			<p className="text-center">Login</p>
 			<p className="text-center">Blog</p>
 			<p className="text-center">Artists</p>
--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -3,8 +3,10 @@ import Navbar from '../components/Navbar'
 
 const Landing: React.FC = () => {
     return(
-        <div className="px-8 py-4 lg:px-16 bg-gray-900">
-			<Navbar />            
+        <div className="bg-gray-900">
+        	<div className="border border-gray-900">
+				<Navbar />            
+        	</div>
         </div>
     )
 }
